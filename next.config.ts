aea1c3c66ece2e7153ce86fcd0f1f6d2865ccbff
David Ernst <git@dsernst.com>
2025-10-31 21:45:45 -0700

    Redirect /goenka_recordings to prev blog's new url

--- next.config.ts
+++ next.config.ts
@@ -9,6 +9,11 @@ const nextConfig: NextConfig = {
         destination: 'http://quotes.dsernst.com',
         permanent: true,
       },
+      {
+        source: '/goenka_recordings/:path*', // wildcard match
+        destination: 'https://blog.dsernst.com/goenka_recordings/:path*', // preserve subpath
+        permanent: true,
+      },
     ]
   },
 }
